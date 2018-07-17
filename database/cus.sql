d5b0518fa978e3e11f4e9d3e04e6143191488a6a
lehuy <leduchuy131096@gmail.com>
2018-07-17 15:53:07 +0700

    Update sql

--- database/cus.sql
+++ database/cus.sql
@@ -2,10 +2,10 @@
 -- version 4.8.2
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th7 17, 2018 lúc 10:03 AM
--- Phiên bản máy phục vụ: 10.1.34-MariaDB
--- Phiên bản PHP: 7.2.7
+-- Host: 127.0.0.1
+-- Generation Time: Jul 17, 2018 at 10:52 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,49 +19,49 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `cus`
+-- Database: `cus`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `info`
+-- Table structure for table `info`
 --
 
 CREATE TABLE `info` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `id_url` int(10) UNSIGNED DEFAULT NULL,
-  `time_click` datetime DEFAULT CURRENT_TIMESTAMP,
+  `id_url` int(10) UNSIGNED NOT NULL,
+  `time_click` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `browser` tinyint(4) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `url`
+-- Table structure for table `url`
 --
 
 CREATE TABLE `url` (
   `id` int(10) UNSIGNED NOT NULL,
   `url` varchar(256) NOT NULL,
-  `short_url` varchar(10) NOT NULL,
+  `short_url` varchar(6) NOT NULL,
   `num_click` int(11) DEFAULT '0',
-  `time_create` datetime DEFAULT CURRENT_TIMESTAMP
+  `time_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `info`
+-- Indexes for table `info`
 --
 ALTER TABLE `info`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_url` (`id_url`);
 
 --
--- Chỉ mục cho bảng `url`
+-- Indexes for table `url`
 --
 ALTER TABLE `url`
   ADD PRIMARY KEY (`id`),
@@ -69,27 +69,27 @@ ALTER TABLE `url`
   ADD UNIQUE KEY `short_url` (`short_url`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `info`
+-- AUTO_INCREMENT for table `info`
 --
 ALTER TABLE `info`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT cho bảng `url`
+-- AUTO_INCREMENT for table `url`
 --
 ALTER TABLE `url`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- Constraints for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `info`
+-- Constraints for table `info`
 --
 ALTER TABLE `info`
   ADD CONSTRAINT `info_ibfk_1` FOREIGN KEY (`id_url`) REFERENCES `url` (`id`);
