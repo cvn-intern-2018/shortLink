9344d6218148be420a853f72fc48c5b1a1e64955
Doan Ngoc Dang <d001297@cb.local>
2018-07-17 14:29:52 +0700

    template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,89 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Shorter Link</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Loading Bootstrap -->
+    <link href="assets/sass/bootstrap.css" rel="stylesheet">
+
+    <!-- Edit CSS -->
+    <link href="assets/sass/default.css" rel="stylesheet">
+    <link href="assets/sass/main.css" rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="assets/sass/font-awesome.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,900,700,600,300,200" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:100,300,400,700" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+</head>
+<body>
+
+<div id="page" class="page">
+
+    <!-- header -->
+    <header class="header8" id="home">
+
+        <nav role="navigation" class="navbar navbar-default">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="#" class="navbar-brand brand"><img src="assets/images/ogp.png" alt="" height="40" width="50"><span>Cybozu</span><span>Company</span></a>
+                </div>
+            </div>
+        </nav>
+
+        <div class="intro center-content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-10 col-md-offset-1 col-sm-10 col-sm-offset-1 col-xs-12">
+                        <div class="email text-center"><i class="fa fa-link" aria-hidden="true"></i></div>
+                        <p class="tag-line text-center">Ready To Launch</p>
+                        <h1 class="pull-stop text-center">Get Latest Newsletters</h1>
+                        <form class="login-reg subscribe text-center form-inline">
+                            <div class="row margin-bottom-0">
+                                <div class="col-md-12 col-sm-12">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control input-lg" id="exampleInputEmail2" placeholder="Your Email*">
+                                    </div>
+                                    <button type="submit" class="btn btn-info button">Subscribe</button>
+                                </div>
+                                <div class="col-md-12">
+                                    <div class="sign-tag m-auto"><p>Let Us Know When It Luanched | <a href="#">Hire Me!</a></p></div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div><!-- row -->
+
+            </div><!-- intro -->
+
+        </div><!-- container -->
+
+    </header><!-- header -->
+    <footer class="footer8">
+        <div class="container">
+            <div class="col-md-12">
+                <p class="pull-left small">© Copyright - <a href="https://scripteden.com/download/creator-builder">Eden Builder</a> and Scripeden Network 2018</p>
+                <p class="pull-right small">Made with <i class="fa fa-heart"></i> in London</p>
+            </div>
+        </div>
+    </footer>
+</div><!-- /#page -->
+
+
+<!--=== Load JS here for greater good ====-->
+<script src="assets/js/jquery-1.8.3.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/isotope.pkgd.js"></script>
+<script src="assets/js/jquery.countdown.min.js"></script>
+<script src="assets/js/jquery.flexslider.js"></script>
+<script src="assets/js/jquery.nivo.slider.pack.js"></script>
+<script src="assets/js/portfolio-custom1.js"></script>
+<script src="assets/js/portfolio-custom2.js"></script>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 //
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/welcome', function () {
+    return view('welcome');
+});
 Route::get('/', 'home@index');
