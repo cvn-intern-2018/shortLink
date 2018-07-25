cb6c0212185f4354e39398686e25e5c8be9e6bbe
lehuy <leduchuy131096@gmail.com>
2018-07-25 11:11:46 +0700

    create migration

--- database/migrations/2018_07_25_031048_url.php
+++ database/migrations/2018_07_25_031048_url.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class Url extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,13 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('url', function (Blueprint $table){
+            $table->increments('id');
+            $table->text('original_url');
+            $table->string('shorten_url', 20);
+            $table->timestamp('time_create');
+            $table->unique('shorten_url');
+
         });
     }
 
@@ -27,6 +30,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('url');
     }
 }
