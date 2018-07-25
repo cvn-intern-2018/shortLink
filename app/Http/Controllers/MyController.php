005eacd182cb4af715c37ed8d713859fbccd9cb8
lehuy <leduchuy131096@gmail.com>
2018-07-25 09:47:14 +0700

    aaa

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class MyController extends Controller
+{
+    public function function1(){
+       return view('chart');
+    }
+}
--- database/cus.sql
+++ database/cus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2018 at 10:52 AM
+-- Generation Time: Jul 25, 2018 at 04:20 AM
 -- Server version: 10.1.34-MariaDB
 -- PHP Version: 7.2.7
 
@@ -25,28 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `info`
---
-
-CREATE TABLE `info` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `id_url` int(10) UNSIGNED NOT NULL,
-  `time_click` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `browser` tinyint(4) DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `url`
 --
 
 CREATE TABLE `url` (
   `id` int(10) UNSIGNED NOT NULL,
-  `url` varchar(256) NOT NULL,
-  `short_url` varchar(6) NOT NULL,
-  `num_click` int(11) DEFAULT '0',
+  `original_url` text NOT NULL,
+  `shorten_url` varchar(20) NOT NULL,
   `time_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `url_info`
+--
+
+CREATE TABLE `url_info` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `id_url` int(10) UNSIGNED NOT NULL,
+  `time_click` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `browser` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -54,29 +53,22 @@ CREATE TABLE `url` (
 --
 
 --
--- Indexes for table `info`
---
-ALTER TABLE `info`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_url` (`id_url`);
-
---
 -- Indexes for table `url`
 --
 ALTER TABLE `url`
   ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `url` (`url`),
-  ADD UNIQUE KEY `short_url` (`short_url`);
+  ADD UNIQUE KEY `shorten_url` (`shorten_url`);
+
+--
+-- Indexes for table `url_info`
+--
+ALTER TABLE `url_info`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_url` (`id_url`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `info`
---
-ALTER TABLE `info`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `url`
@@ -85,14 +77,20 @@ ALTER TABLE `url`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `url_info`
+--
+ALTER TABLE `url_info`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `info`
+-- Constraints for table `url_info`
 --
-ALTER TABLE `info`
-  ADD CONSTRAINT `info_ibfk_1` FOREIGN KEY (`id_url`) REFERENCES `url` (`id`);
+ALTER TABLE `url_info`
+  ADD CONSTRAINT `url_info_ibfk_1` FOREIGN KEY (`id_url`) REFERENCES `url` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
