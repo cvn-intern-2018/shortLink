4dda1330b11d26df7eaccca64872e21ebe8585ae
lehuy <leduchuy131096@gmail.com>
2018-07-27 10:03:05 +0700

    a

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -2,12 +2,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
+
 
 class MyController extends Controller
 {
-    public function function1(){
-       return view('chart');
+    public function function1(Request $request){
+        $ip_address = $request->ip();
+        echo '<br>Địa chỉ IP người dùng: ' . $ip_address;
     }
 }
