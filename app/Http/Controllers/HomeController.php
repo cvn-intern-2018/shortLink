06f296cdfdc790f6f4f771f6391b20ef000a1e68
Doan Ngoc Dang <d001297@cb.local>
2018-07-27 17:05:57 +0700

    m

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-<<<<<<< HEAD
+
 
 namespace App\Http\Controllers;
 
@@ -16,62 +16,90 @@ class HomeController extends Controller
 
         $url = Url::all();
 
-        $this->debug_to_console( $url );
+        $this->debug_to_console($url);
 
-        return view('home',['url' => 12314]);
+        return view('home', ['url' => 12314]);
     }
+
     public function getAllUrl()
     {
         $urls = Url::all();
 
-        $this->debug_to_console( $urls );
+        $this->debug_to_console($urls);
     }
+
     public function index()
     {
         $url = 'https://github.com/cotdp/php-rc4/blob/master/rc4.php';
-        $original_url = Url::where('url_original',$url)->get()->toJson();//or toArray or
+        $original_url = Url::where('url_original', $url)->get()->toJson();//or toArray or
         var_dump($original_url);
     }
+
     public function getURLShortener()
     {
         $url = 'https://github.com/cotdp/php-rc4/blob/master/rc4.php';
-        $original_url = Url::where('url_shorten',$url)->get();
+        $original_url = Url::where('url_shorten', $url)->get();
         var_dump($original_url);
     }
-    public function addURLShortener(){
 
-    }
-
-
-
-    function debug_to_console( $data ) {
-        $output = $data;
-        if ( is_array( $output ) )
-            $output = implode( ',', $output);
-
-        echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
-    }
-=======
-namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
-
-class HomeController extends Controller
-{
-    public function index()
+    public function addURLShortener()
     {
-        return view('home');
+
     }
-    public function ValidLink($url) {
+
+    public function ValidLink($url)
+    {
 
         $regular = '/^(http:\/\/|https:\/\/)?[\w]+(\.{1}[\w]+)+(\S*)$/';
 
-        if (strlen($url) < 2048 && $check = preg_match($regular, $url)){
+        if (strlen($url) < 2048 && $check = preg_match($regular, $url)) {
             return true;
         } else {
             return false;
         }
     }
 
->>>>>>> 982bb096b500376e494a7a4a74c0f127de479852
+    function debug_to_console($data)
+    {
+        $output = $data;
+        if (is_array($output))
+            $output = implode(',', $output);
+
+        echo "<script>console.log( 'Debug Objects: " . $output . "' );</script>";
+    }
+
+    public function encode($id) {
+        $alphabet='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-.';
+        $length = strlen($alphabet);
+        $shortlink = '';
+
+        while($id > 0) {
+            $shortlink = $shortlink .$alphabet[(int)($id % $length)]  ;
+            $id = (int)($id / $length);
+        }
+        while(strlen($shortlink) < 6) {
+            $shortlink = '0' .$shortlink;
+        }
+        $shortlink = $alphabet[rand(0,63)] . $shortlink;
+        return  $shortlink;
+
+    }
+
+    public function short(Request $req)
+    {
+        $old_id = DB::table('url')->max('id');
+
+        $domain = "http://cus.dev.cybozu.xyz/";
+        $short = home::encode($old_id + 1);
+        $short_url = $domain . $short;
+
+        $url = new Url();
+
+        $url->url_original = $req->org_url;
+        $url->url_shorten =  $short_url;
+        $url->url_info = "OK";
+        $url->save();
+
+        return redirect('home');
+    }
 }
