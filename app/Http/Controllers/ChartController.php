6cfe2f4dc2ec14e081a22e8aae8572d1d6d7ae2b
lehuy <leduchuy131096@gmail.com>
2018-08-03 14:53:09 +0700

    'merge'

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -12,18 +12,17 @@ class ChartController extends Controller
     //
     public function index(Request $request)
     {
-        $url_shorter = $request->url_shorten;
-        //var_dump($url_shorter_plus);
+        $url_shorten = $request->url_shorten;
 
-        $url = Url::where('url_shorten', $url_shorter)->first();
+        $url = Url::GetByUrlShorten( $url_shorten)->first();
 
-        if(is_null($url))
+        if (is_null($url))
             return view('error.404');
         $url_original_link = $url->url_original;
         $url_short_link = $url->url_shorten;
         $created_at = date_format(date_create($url->created_at), 'd-m-Y');
         $countClickedTime = 0;
-        $access = Access::Where('id', $url->id)->get();
+        $access = Access::getById($url->id)->get();
         if (!is_null($access)) {
             foreach ($access as $item) {
                 $countClickedTime = $countClickedTime + substr_count($item->clicked_time, ' ') + 1;
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-
 Route::get('/chart', 'ChartController@index');
 Route::get('/', 'HomeController@index');
 Route::get('data', 'HomeController@returnData');
-
-Route::get('/demo', 'HomeController@getBrowser');
+Route::get('/demo', 'HomeController@test');
 Route::post('short', 'HomeController@shortURL');
 Route::get('pagenotfound','HomeController@pageNotFound');
-
-
-Route::get('pagenotfound','HomeController@pageNotFound');
-
 Route::get('/{url}','HomeController@redirectUrl');
 
