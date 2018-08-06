460bef8c55bee4032b2454aafad069ae1b3b0e56
lehuy <leduchuy131096@gmail.com>
2018-08-06 08:58:57 +0700

    'add_function_validcustomizeinput'

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,3 @@ Route::post('/chart/statistics', 'ChartController@getDataStatistics');
 Route::get('pagenotfound', 'HomeController@pageNotFound');
 Route::get('/{url_shorten}+', 'ChartController@index');
 Route::get('/{url}', 'HomeController@redirectUrl');
-Route::get('/demo','HomeController@test');
