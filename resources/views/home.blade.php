7f526ad6c1a8fa0829663c131388e2363e76d4ee
lehuy <leduchuy131096@gmail.com>
2018-08-06 08:24:51 +0700

    'merge'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
                                         </div>
                                         <div class="customize form-group">
                                             <h4>Customize short URL (Optional)</h4>
-                                            <p>{{ $domain }}/</p><input maxlength="20" type="text" id="custom_url" name="custom_url" placeholder="Max length is 20 letters">
+                                            <p>{{ $domain }}/</p><input maxlength="20" type="text" id="custom_url" name="custom_url" placeholder="7-20 characters( alphabet ,numeric, hyphen ,underscore)">
                                         </div>
                                         <div class="form-group row" id="short-notify">
 
