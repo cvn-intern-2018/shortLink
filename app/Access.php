bf3c628224914d922fcbaf9a1dcffc5dc4882504
ngdang <1412128@student.hcmus.edu.vn>
2018-08-07 14:20:24 +0700

    fix bug char 100%

--- app/Access.php
+++ app/Access.php
@@ -9,6 +9,11 @@ class Access extends Model
     protected $table = 'access';
     public $timestamps = false;
 
+    /**
+     * @param $url_id
+     * @param $browser
+     * @param $click_time
+     */
     function saveData($url_id, $browser, $click_time)
     {
         $this->url_id = $url_id;
@@ -17,13 +22,61 @@ class Access extends Model
         $this->save();
     }
 
+    /**
+     * @param $query
+     * @param $url_id
+     * @return mixed
+     */
     function scopeGetById($query, $url_id)
     {
         return $query->where('url_id', $url_id );
     }
 
+    /**
+     * @param $query
+     * @param $browser
+     * @return mixed
+     */
     function scopeGetByBrowser($query, $browser)
     {
         return $query->where('browser', $browser);
+    }
+
+    /**
+     * @param $query
+     * @param $url_id
+     * @return int
+     */
+    function scopeGetTotalClickUrlShort($query, $url_id){
+        $clicked_time_total = 0;
+        $records = $query->where('url_id', $url_id )->get();
+        foreach ($records as $item) {
+            $arr_clicked_time = explode(' ',$item->clicked_time);
+            $clicked_time_total += count($arr_clicked_time);
+        }
+        return $clicked_time_total;
+    }
+
+    /**
+     * @param $query
+     * @param $url_id
+     * @return array
+     */
+    function scopeGetArrTimerUrlShort($query, $url_id){
+        $arr_data_browser =[];
+        $browser = config('constants.browser');
+        foreach ($browser as $key => $value) {
+            $obj_browser = (object)[
+                'browser_name' => $key,
+                'arr_click_time' => []
+            ];
+            array_push($arr_data_browser, $obj_browser);
+        }
+        $record_access = $query->where('url_id', $url_id )->get();
+        foreach ($record_access as $item) {
+            $arr_clicked_time = explode(' ',$item->clicked_time);
+            $arr_data_browser[$item->browser]->arr_click_time = $arr_clicked_time;
+        }
+        return $arr_data_browser;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 Route::get('/', 'HomeController@index');
 Route::post('short', 'HomeController@shortURL');
+Route::post('chart/sort', 'ChartController@sortTimeToStatistics');
 Route::get('pagenotfound', 'HomeController@pageNotFound');
 Route::get('/{url_shorten}+', 'ChartController@index');
 Route::get('/{url}', 'HomeController@redirectUrl');
